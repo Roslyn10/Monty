f7d4b8a395cabb84a5b24df403629171a3006d82
Roslyn10 <roslynclewis1004@gmail.com>
2024-01-31 15:15:28 +0200

    Adding nop function to the header and adding the nop code

--- monty.h
+++ monty.h
@@ -38,5 +38,7 @@ typedef struct instruction_s
         void (*f)(stack_t **stack, unsigned int line_number);
 } instruction_t;
 
+/*Functions*/
+void nop(void);
 
 #endif /**MONYY_H**/
---
+++ nop.c
@@ -0,0 +1,12 @@
+#include "monty.h"
+
+/**
+ * nop - An opcode that does noting
+ * Description - Doesnt do anything
+ * Return - Nothing
+ */
+
+void nop(void)
+{
+	return;
+}
