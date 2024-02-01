4acbb41dadc1410376db015e638bce1d8b977a33
Roslyn10 <roslynclewis1004@gmail.com>
2024-02-01 12:37:25 +0200

    Updating the monty.c file and adding other codes

--- monty.c
+++ monty.c
@@ -13,6 +13,9 @@
 int main(int argc, char **argv[])
 {
 	FILE *file = NULL;
+	char line = NULL;
+	char *opcode, *argument;
+	stack_t *stack = NULL;
 
 	if (argc != 2)
 	{
@@ -28,5 +31,20 @@ int main(int argc, char **argv[])
 		fprintf(stderr, "Error: Can't open file file\n");
 		exit(EXIT_FAILURE);
 	}
+
+	while (fgets(line, sizeof(line), file))
+	{
+		opcode = strtok(line, " \t\n");
+		if (opcode == NULL || *opcode == "#")
+		{
+			continue;
+		}
+
+		argument = strtok(NULL, " \t\n");
+
+		if (strcmp(opcode, "push") == 0)
+		{
+
+
 	return (0);
 }
