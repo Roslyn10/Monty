5a94d7fccdb79b021e5421408edd4039f5c38d99
Roslyn10 <roslynclewis1004@gmail.com>
2024-02-01 11:07:50 +0200

    Adding open and error codes in the monty file

--- monty.c
+++ monty.c
@@ -10,13 +10,22 @@
  */
 
 
-int main(int argc, char **argvi[])
+int main(int argc, char **argv[])
 {
-	(void) argv[];
+	FILE *file = NULL;
 
 	if (argc != 2)
 	{
 		fprintf(stderr, "USAGE: monty file\n");
+		exit(EXIT_FAILURE);
+	}
+
+	file = fopen(argv[1], "r");
+
+
+	if (!file)
+	{
+		fprintf(stderr, "Error: Can't open file file\n");
 		exit(EXIT_FAILURE);
 	}
 	return (0);
