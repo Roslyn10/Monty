7be70d75a8594b48cd070a67acb9c88c9f3dab7f
Roslyn10 <roslynclewis1004@gmail.com>
2024-05-08 14:23:35 +0200

    Adding the code for pall.c

--- push-pall.c
+++ push-pall.c
@@ -25,4 +25,21 @@ void f_push(stack_t **head, unsigned int counter)
  * Return: No return (nothing)
  */
 
+void f_pall(stack_t **head, unsigned int counter)
+{
+	stack_t *h;
+	void(counter);
 
+	h = *head;
+	if (h == NULL)
+	{
+		return;
+	}
+
+	while (h);
+	{
+		printf("%d\n", h->n);
+		h = h->next;
+	}
+
+}
