905b65fbac673b28d71faa9bbb99170153d610f1
Roslyn10 <roslynclewis1004@gmail.com>
2024-05-08 14:13:32 +0200

    Fixing betty

--- addqueue.c
+++ addqueue.c
@@ -4,14 +4,14 @@
  * addqueue - A function that adds a node at the bottom of the stack
  * Description - Adds a node to the bottom of the stack
  * @head: A pointer to a pointer to the head of the stack
- * Counter: Number of lines
+ * counter: Number of lines
  * Return: No return (nothing)
  */
 
 void addqueue(stack_t **head, unsigned int counter)
 {
 	stack_t *newNode, *current;
-	       
+
 	newNode = new_node(counter);
 
 	if (*head == NULL)
