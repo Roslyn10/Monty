e44db35cbfaa2816877c38c72ef63e51f86dca09
Roslyn10 <roslynclewis1004@gmail.com>
2024-05-20 15:15:59 +0200

    Fixing Error

--- monty.c
+++ monty.c
@@ -97,7 +97,7 @@ int main(int argc, char *argv[])
 	}
 	if (file == NULL)
 	{
-		fprintf(stderr, "Error : Can't open file %s\n", argv[1]);
+		fprintf(stderr, "Error: Can't open file %s\n", argv[1]);
 		exit(EXIT_FAILURE);
 	}
 	while (fgets(opcode, sizeof(opcode), file) != NULL)
